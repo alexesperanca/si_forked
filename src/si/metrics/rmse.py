5d011c914f599d19d1cd489fe9085b2912a82eae
alexandre.esperanca <alexandre.esperanca@ozono.pt>
2022-10-31 17:55:10 +0000

    Files added from class 5

--- src/si/metrics/rmse.py
+++ src/si/metrics/rmse.py
@@ -1,13 +1,14 @@
 import numpy as np
 
+
 def rmse(y_true: list, y_pred: list) -> float:
-  """Root Mean Square Error calculation.
+    """Root Mean Square Error calculation.
 
-  Args:
-      y_true (list): Real Y axis values. 
-      y_pred (list): Predicted Y axis values. 
+    Args:
+        y_true (list): Real Y axis values.
+        y_pred (list): Predicted Y axis values.
 
-  Returns:
-      float: RMSE error.
-  """
-  return np.sqrt(np.sum(np.subtract(y_true, y_pred)**2) / len(y_true))
+    Returns:
+        float: RMSE error.
+    """
+    return np.sqrt(np.sum(np.subtract(y_true, y_pred) ** 2) / len(y_true))
